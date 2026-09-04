0962ef0aae3edc37944864ec400d4e65e29fe27b
Sourav <313756942+SouravShee123@users.noreply.github.com>
2026-09-04 16:23:44 +0530

    Add the logic for the game using if...else
    Called function inside another function

--- script.js
+++ script.js
@@ -24,10 +24,43 @@ function getHumanChoice() {
         return "scissors"
     }
 }
-console.log(getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
-function playRound(humanChoice,computerChoice){
-    humanChoice.toLowerCase();
+function playRound(humanSelection,computerChoice){
+    let humanInput = humanSelection.toLowerCase();
+    if(humanInput==="rock" && computerChoice==="paper"){
+        console.log("You lose! Paper beats Rock");
+        computerScore++;
+    }
+    else if(humanInput==="paper" && computerChoice==="scissors"){
+        console.log("You lose! Scissors beats Paper");
+        computerScore++;
+    }
+    else if(humanInput==="scissors" && computerChoice==="rock"){
+        console.log("You lose! Rock beats Scissors");
+        computerScore++;
+    }
+    else if(humanInput==="rock" && computerChoice==="scissors"){
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+    }
+    else if(humanInput==="paper" && computerChoice==="rock"){
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    }
+    else if(humanInput==="scissors" && computerChoice==="paper"){
+        console.log("You win! Scissors beats Paper");
+        humanScore++;
+    }
+    else{
+        console.log("Its a draw! Lets go again");
+
+    }
+    
 }
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
