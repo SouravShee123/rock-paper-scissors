99dddb4282a7592f61af18ac9b2987d1c30a8164
Sourav <313756942+SouravShee123@users.noreply.github.com>
2026-09-04 15:44:45 +0530

    Add getHumanChoice function with prompt input

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello World");
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random()*3);
         if(randomNumber===0){
@@ -10,4 +10,24 @@ function getComputerChoice() {
         else{
             return "scissors"
         }
+}
+
+function getHumanChoice() {
+    let myChoice = parseInt(prompt("Enter 0 for rock, 1 for paper and 2 for scissors"));
+    if(myChoice === 0){
+        return "rock";
+    }
+    else if(myChoice === 1){
+        return "paper";
+    }
+    else{
+        return "scissors"
+    }
+}
+console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+function playRound(humanChoice,computerChoice){
+    humanChoice.toLowerCase();
 }
